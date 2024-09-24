4ae69d83d71ad782f728ceba8c3b53a75e5ab4ba
Praveen Neelanathan <praveenneelanathan@Praveens-MacBook-Pro.local>
2024-09-25 02:35:42 +0530

    LDT-953 EX-1 - Swapping Varaibles.

---
+++ JS-Basics/index.js
@@ -0,0 +1,11 @@
+// EX - 2 Swapping Variables
+
+let x = 'red';
+let y = 'blue';
+
+let z = x; // z = 'red'
+x = y; // x = 'blue'
+y = z; // y = 'red'
+
+console.log(x); // blue
+console.log(y); // red 
