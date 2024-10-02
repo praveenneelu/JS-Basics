89fb04c818f8a9340fe35b79873a1c20e4b4c4d8
Praveen Neelanathan <praveenneelanathan@Praveens-MacBook-Pro.local>
2024-10-02 18:24:11 +0530

    EX-3 - check-speed-limit.

--- JS-Basics/index.js
+++ JS-Basics/index.js
@@ -1,11 +1,24 @@
-// EX - 1 Swapping Variables
+// Write a function for checkspeed limit of vehicle.
+// Speed Limit = 70
+// More than 5 speed(i.e 75) = 1 point
+// 12 points = suspended.
 
-let x = 'red';
-let y = 'blue';
+checkspeed(77);
+function checkspeed(speed) {
+    const speedLimit = 70;
+    const kmPerPoint = 5;
+    if (speed <= (speedLimit + kmPerPoint)) {
+        console.log("Good");
+        return;
+    }
+    
+    const points = Math.floor((speed - speedLimit) / kmPerPoint) ;
+    if (points > 12) {
+        console.log("License is suspended");
+    }
+    else {
+        console.log('Points', points)
+    }
+    
 
-let z = x; // z = 'red'
-x = y; // x = 'blue'
-y = z; // y = 'red'
-
-console.log(x); // blue
-console.log(y); // red 
+}
